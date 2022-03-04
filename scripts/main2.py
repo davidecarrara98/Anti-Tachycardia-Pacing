acffc1ad79b6e67da6445471214badf04171b7ca
Luca Sosta <lucasosta@libero.it>
2022-03-04 16:18:01 +0100

    ef

--- scripts/main2.py
+++ scripts/main2.py
@@ -3,7 +3,7 @@ import utils2
 import matplotlib.pyplot as plt
 
 nu2 = 0.012400
-grid = 'fine'
-#utils2.save_first_450(nu2, refined_grid=False)
-signal = utils2.generate_last_350(nu2, ICD_time=500, ICD_duration=10, refined_grid=False)
+grid = False #True is fine
+#utils2.save_first_450(nu2, refined_grid=grid)
+signal = utils2.generate_last_350(nu2, ICD_time=500, ICD_duration=10, refined_grid=grid)
 plt.plot(signal[0,1,:],signal[0,0,:], "r")
