c5a14094826d59143a8bac50161bd28423b9d1e7
Luca Sosta <lucasosta@libero.it>
2022-03-04 19:11:09 +0100

    main2_all

---
+++ main1.py
@@ -0,0 +1,29 @@
+import numpy as np
+import sys
+
+sys.path.append("scripts")
+from BayesianOptimizer import BayesOptimizer
+from utils1 import l1_norm, l2_norm, l12_norm
+
+# Set seed for reproducibility
+np.random.seed(2304)
+# nu2 can belong to [0.0116,0.0124]
+patients = np.load('signals_3_patients.npy')
+
+# COARSE GRID - estimated with MSE: 0.0124 - estimated with MAE: 0.0124 - estimated with comb: 0.0124
+#patient1 = np.array(patients[0])
+#bayes_opt1 = BayesOptimizer(patient1, name_patient="Patient 1", niter=10, k=3, T=450, load_all=True,
+#                            min_iter=1, error_function=l2_norm, refined_grid=True)
+#nu1, mod1 = bayes_opt1.optimize()
+
+# COARSE GRID - estimated with MSE: 0.01205325 - estimated with MAE: 0.01204525 - estimated with comb: 0.01204605
+#patient2 = np.array(patients[1])
+#bayes_opt2 = BayesOptimizer(patient2, name_patient="Patient 2", niter=10, k=3, T=450, load_all=True,
+#                            min_iter=1, error_function=l2_norm, refined_grid=True)
+#nu2, mod2 = bayes_opt2.optimize()
+
+# COARSE GRID - estimated with MSE: 0.01183063 - estimated with MAE: 0.01182663 - estimated with comb: 0.01182743
+patient3 = np.array(patients[2])
+bayes_opt3 = BayesOptimizer(patient3, name_patient="Patient 3", niter=10, k=3, T=450, load_all=True,
+                            min_iter=2, error_function=l2_norm, refined_grid=True)
+nu3, mod3 = bayes_opt3.optimize()
---
+++ main2.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+sys.path.append("scripts")
+from utils2 import save_first_450, generate_last_350, load_nu
+
+patient = 3
+nu2 = load_nu(error_type = "MSE")[patient]
+
+grid_name = "fine"
+grid = True if grid_name == "fine" else False 
+
+#save_first_450(nu2, refined_grid=grid)
+
+new_signal = generate_last_350(nu2, ICD_time=820, ICD_duration=9, refined_grid=grid)
+ori_signal = np.load("signals_3_patients.npy")
+
+# add save
+
+plt.plot(ori_signal[patient-1,1,:],ori_signal[patient-1,0,:],"g")
+plt.plot(new_signal[0,1,:],new_signal[0,0,:],"b")
+plt.plot([450,450],[-1.5,1],"r")
